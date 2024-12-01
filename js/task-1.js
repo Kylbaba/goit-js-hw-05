9a82b3bbd08ba26ff8356a5b4fbaa5627768c64c
Tatiana <petrenkotanya18@gmail.com>
2024-12-02 01:04:40 +0200

    formed tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,53 @@
-const books = [
-    {
-      title: "The Last Kingdom",
-      author: "Bernard Cornwell",
-      rating: 8.38,
-    },
-    {
-      title: "Beside Still Waters",
-      author: "Robert Sheckley",
-      rating: 8.51,
-    },
-    {
-      title: "The Dream of a Ridiculous Man",
-      author: "Fyodor Dostoevsky",
-      rating: 7.75,
-    },
-    { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-    {
-      title: "The Dreams in the Witch House",
-      author: "Howard Lovecraft",
-      rating: 8.67,
-    },
-  ];
-  const MIN_BOOK_RATING = 8;
-  
-  const names = books
-    .filter(book => book.rating > MIN_BOOK_RATING)
-    .map((book) => book.author)
-  .toSorted((a,b) => a.localeCompare(b));
-  
+// Задача 1. Імена користувачів
+// Напиши стрілочну функцію getUserNames(users), яка прийматиме один параметр users — масив об’єктів користувачів. Функція має повертати масив імен усіх користувачів (властивість name) із масиву users.
+
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її викликів.
+
+
+// console.log(
+//   getUserNames([
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     balance: 2811
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     balance: 3821
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     balance: 3793
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     balance: 2278
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     balance: 3951
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     balance: 1498
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     balance: 2764
+//   },
+// ])
+// ); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+
+// На що буде звертати увагу ментор при перевірці:
+
+// Оголошена змінна getUserNames
+// Змінній getUserNames присвоєна стрілочна функція з параметром (users).
+// Для перебирання параметра users використовується метод map()
+// Виклик функції із зазначеним масивом користувачів повертає масив ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+// Виклик функції з випадковими, але валідними аргументами повертає правильне значення
